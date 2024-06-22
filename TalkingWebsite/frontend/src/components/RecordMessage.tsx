130c77b87d92c412567a36a069f1c08ce2af36db
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-22 15:53:00 +0000

    MessageRecording barebones funcion

--- TalkingWebsite/frontend/src/components/RecordMessage.tsx
+++ TalkingWebsite/frontend/src/components/RecordMessage.tsx
@@ -1,2 +1,12 @@
 import { ReactMediaRecorder } from "react-media-recorder";
 import RecordIcon from "./RecordIcon";
+
+type Props = {
+  handleStop: any;
+};
+
+const RecordMessage = ({ handleStop }: Props) => {
+  return ();
+};
+
+export default RecordMessage;
