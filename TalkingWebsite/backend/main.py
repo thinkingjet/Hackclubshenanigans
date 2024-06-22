387c0116d9e5e128a56f519c4515067ec5c6b728
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-22 09:24:43 +0000

    testing and writing a main.py file that handles the backend fast api

--- TalkingWebsite/backend/main.py
+++ TalkingWebsite/backend/main.py
@@ -14,3 +14,49 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 
+
+
+app = FastAPI()
+
+# CORS Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4174",
+    "http://localhost:4173",
+    "http://localhost:3000",
+]
+
+
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_headers=["*"],
+    allow_methods=["*"],
+    allow_credentials=True,
+)
+
+@app.get("/check")
+async def check_health():
+    return {"response": "working properly"}
+
+
+#testing
+@app.get("/post-audio-get/")
+async def get_audio():
+    audio_input = open("voice.mp3", "rb")
+    message_decoded = convert_audio_to_text(audio_input)
+    message_decoded = convert_audio_to_text(audio_input)
+    if not message_decoded:
+        raise HTTPException(status_code=400, detail="Failed to decode audio")
+    chat_response = get_chat_response(message_decoded)
+    store_messages(message_decoded, chat_response)
+    print(chat_response)
+    audio_output = convert_text_to_speech(chat_response)
+    if not audio_output:
+        raise HTTPException(status_code=400, detail="Failed audio output")
+    def iterfile():
+       yield audio_output
+    return StreamingResponse(iterfile(), media_type="application/mpeg")
+
