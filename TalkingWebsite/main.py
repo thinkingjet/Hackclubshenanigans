d8d9dce8a0d9072898edc9ac5ca413537b5bb1c1
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-22 18:28:37 +1000

    adding imports

---
+++ TalkingWebsite/main.py
@@ -0,0 +1,16 @@
+# need to run the backend each time:
+# uvicorn main:app --reload
+
+
+
+
+import openai, os
+from functions.text_to_speech import convert_text_to_speech
+from functions.openai_requests import convert_audio_to_text, get_chat_response
+from functions.database import store_messages, reset_messages
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
