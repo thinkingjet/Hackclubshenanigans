2714b4303ae25ad603fc4ce12822bdbb3dd68847
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-23 06:40:42 +0000

    Finishing up title.tsx

--- TalkingWebsite/frontend/src/components/Title.tsx
+++ TalkingWebsite/frontend/src/components/Title.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import axios from "axios";
 
+type Props = {
+  setMessages: any;
+};
 
 function Title({ setMessages }: Props) {
   const [isResetting, setIsResetting] = useState(false);
@@ -18,7 +21,13 @@ function Title({ setMessages }: Props) {
         if (res.status == 200) {
           setMessages([]);
         }
+      })
+      .catch((err) => {});
 
+    setIsResetting(false);
+  };
+
+  return (
 <div className="flex justify-between items-center w-full p-4 text-white font-bold shadow" style={{
       background: 'linear-gradient(to right, #2f2f2f, #1b1b1b, #2f2f2f)', 
       color: '#ffffff', 
@@ -41,4 +50,16 @@ function Title({ setMessages }: Props) {
           strokeWidth={1.5}
           stroke="currentColor"
           className="w-6 h-6"
-        ></svg>
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export default Title;
