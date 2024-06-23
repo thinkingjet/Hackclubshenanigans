5427b42ad9aed8f5d1ce09885cee2417545ed065
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-23 04:34:31 +0000

    editing Title.tsx page

--- TalkingWebsite/frontend/src/components/Title.tsx
+++ TalkingWebsite/frontend/src/components/Title.tsx
@@ -6,3 +6,28 @@ function Title({ setMessages }: Props) {
   
     const resetConversation = async () => {
       setIsResetting(true);}}
+
+
+<div className="flex justify-between items-center w-full p-4 text-white font-bold shadow" style={{
+      background: 'linear-gradient(to right, #2f2f2f, #1b1b1b, #2f2f2f)', 
+      color: '#ffffff', 
+      padding: '20px',
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+      border: '1px solid #ffffff'
+    }}>
+      <div className="italic">Tesla AI Sales Rep</div>
+      <button
+        onClick={resetConversation}
+        className={
+          "transition-all duration-300 text-blue-300 hover:text-pink-500 " +
+          (isResetting && "animate-pulse")
+        }
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        ></svg>
