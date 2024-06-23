c7b6482bbabf5fb331c031c9d4347f1a777bb63b
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-23 03:29:53 +0000

    stuck on this damn bug

--- TalkingWebsite/frontend/src/components/RecordMessage.tsx
+++ TalkingWebsite/frontend/src/components/RecordMessage.tsx
@@ -6,7 +6,30 @@ type Props = {
 };
 
 const RecordMessage = ({ handleStop }: Props) => {
-  return ();
+  return (
+    <ReactMediaRecorder
+      audio
+      onStop={handleStop}
+      render={({ status, startRecording, stopRecording }) => (
+        <div className="mt-2">
+          <button
+            onMouseDown={startRecording}
+            onMouseUp={stopRecording}
+            className="bg-black p-4 rounded-full"
+          >
+            <RecordIcon
+              classText={
+                status == "recording"
+                  ? "animate-pulse text-red-500"
+                  : "text-sky-500"
+              }
+            />
+          </button>
+          <p className="mt-2 text-white font-light">{status}</p>
+        </div>
+      )}
+    />
+  );
 };
 
 export default RecordMessage;
--- TalkingWebsite/frontend/src/components/Title.tsx
+++ TalkingWebsite/frontend/src/components/Title.tsx
@@ -1,2 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
+
+function Title({ setMessages }: Props) {
+    const [isResetting, setIsResetting] = useState(false);
+  
+    const resetConversation = async () => {
+      setIsResetting(true);}}
