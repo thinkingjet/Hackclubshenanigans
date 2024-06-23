76c520c5d75806f5673a2e323b2f789aee2c55e3
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-23 14:34:15 +0000

    tryhnna make the whole damn thing work

--- TalkingWebsite/backend/main.py
+++ TalkingWebsite/backend/main.py
@@ -66,3 +66,27 @@ async def reset_conversation():
     reset_messages()
     return {"response": "conversation reset"}
 
+
+
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    audio_input = open(file.filename, "rb")
+    message_decoded = convert_audio_to_text(audio_input)
+    if not message_decoded:
+        raise HTTPException(status_code=400, detail="Failed to decode audio")
+    chat_response = get_chat_response(message_decoded)
+    store_messages(message_decoded, chat_response)
+    if not chat_response:
+        raise HTTPException(status_code=400, detail="Failed chat response")
+    print(chat_response)
+    audio_output = convert_text_to_speech(chat_response)
+    if not audio_output:
+        raise HTTPException(status_code=400, detail="Failed audio output")
+    def iterfile():
+        yield audio_output
+    return StreamingResponse(iterfile(), media_type="application/octet-stream")
+
+
+
