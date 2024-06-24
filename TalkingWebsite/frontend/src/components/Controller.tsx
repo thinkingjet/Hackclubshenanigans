42c01f7eed8939e7ff3e729c88574962c1218c05
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-24 15:30:14 +0000

    controller edits

---
+++ TalkingWebsite/frontend/src/components/Controller.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Title from "./Title";
+import axios from "axios";
+import RecordMessage from "./RecordMessage";
+
+const Controller = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<any[]>([]);
+
+  function createBlobURL(data: any) {
+    const blob = new Blob([data], { type: "audio/mpeg" });
+    const url = window.URL.createObjectURL(blob);
+    return url;
+  }
+
+  const handleStop = async (blobUrl: string) => {
+    setIsLoading(true);
+    const myMessage = { sender: "me", blobUrl };
+    const messagesArr = [...messages, myMessage];
+    fetch(blobUrl)
+      .then((res) => res.blob())
+      .then(async (blob) => {
+        const formData = new FormData();
+        formData.append("file", blob, "myFile.wav");
+
+        await axios
+          .post("http://localhost:8000/post-audio", formData, {
+            headers: {
+              "Content-Type": "audio/mpeg",
+            },
+            responseType: "arraybuffer",
+          })
+          .then((res: any) => {
+            const blob = res.data;
+            const audio = new Audio();
+            audio.src = createBlobURL(blob);
+
+            // const JaniceMessage = { sender: "Janice", blobUrl: audio.src };
+            // messagesArr.push(JaniceMessage);
+            // setMessages(messagesArr);
+
+            setIsLoading(false);
+            audio.play();
+          })
+          .catch((err: any) => {
+            console.error(err);
+            setIsLoading(false);
+          });
+      });
+  };
