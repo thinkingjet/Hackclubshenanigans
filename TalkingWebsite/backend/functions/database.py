1ce7de49d0c331fa83f4d93600d7bd8509dab3c2
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-24 09:17:59 +0000

    database.py edited

--- TalkingWebsite/backend/functions/database.py
+++ TalkingWebsite/backend/functions/database.py
@@ -20,3 +20,51 @@ def get_recent_messages():
     learn_instruction["content"] = learn_instruction["content"] + "Your response will rhetorically question the user"
   else:
     learn_instruction["content"] = learn_instruction["content"] + "Your response will be a bit more serious "
+
+  # Append instruction to message
+  messages.append(learn_instruction)
+
+  # Get last messages
+  try:
+    with open(file_name) as user_file:
+      data = json.load(user_file)
+      
+      # Append last 5 rows of data
+      if data:
+        if len(data) < 5:
+          for item in data:
+            messages.append(item)
+        else:
+          for item in data[-5:]:
+            messages.append(item)
+  except:
+    pass
+
+  
+  # Return messages
+  return messages
+
+
+# Save messages for retrieval later on
+def store_messages(request_message, response_message):
+
+  # Define the file name
+  file_name = "stored_data.json"
+
+  # Get recent messages
+  messages = get_recent_messages()[1:]
+
+  # Add messages to data
+  user_message = {"role": "user", "content": request_message}
+  assistant_message = {"role": "assistant", "content": response_message}
+  messages.append(user_message)
+  messages.append(assistant_message)
+
+  # Save the updated file
+  with open(file_name, "w") as f:
+    json.dump(messages, f)
+
+
+def reset_messages():
+  file_name = "stored_data.json"
+  open(file_name, "w")
