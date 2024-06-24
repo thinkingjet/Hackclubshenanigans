eca15f77d51882ff2a857c71c1cc5af1cfc740a4
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-06-24 08:10:48 +0000

    revamped main and database.py

--- TalkingWebsite/backend/functions/database.py
+++ TalkingWebsite/backend/functions/database.py
@@ -1,3 +1,22 @@
 import os
 import json
 import random
+
+# Save messages for retrieval later on
+def get_recent_messages():
+
+  # Define the file name
+  file_name = "stored_data.json"
+  learn_instruction = {"role": "system", 
+                       "content": "You are a highly experienced, and persuasive Tesla rep and your name is Janice. You always manage to convince people to buy a Tesla. You are cold calling leads who may be intersted in buying a tesla. Talk to them and convince them to buy one.  DO NOT mention that you're a sales rep on the phone call just say you're calling from Tesla. Keep responses under 30 words. "}
+
+  messages = []
+
+  # Add Random Element
+  x = random.uniform(0, 1)
+  if x < 0.2:
+    learn_instruction["content"] = learn_instruction["content"] + "Your response will have some light humour "
+  elif x < 0.5:
+    learn_instruction["content"] = learn_instruction["content"] + "Your response will rhetorically question the user"
+  else:
+    learn_instruction["content"] = learn_instruction["content"] + "Your response will be a bit more serious "
--- TalkingWebsite/backend/main.py
+++ TalkingWebsite/backend/main.py
@@ -38,3 +38,22 @@ app.add_middleware(
 @app.get("/check")
 async def check_health():
     return {"response": "working properly"}
+
+
+#testing
+# @app.get("/post-audio-get/")
+async def get_audio():
+    audio_input = open("voice.mp3", "rb")
+    message_decoded = convert_audio_to_text(audio_input)
+    message_decoded = convert_audio_to_text(audio_input)
+    if not message_decoded:
+        raise HTTPException(status_code=400, detail="Failed to decode audio")
+    chat_response = get_chat_response(message_decoded)
+    store_messages(message_decoded, chat_response)
+    print(chat_response)
+    audio_output = convert_text_to_speech(chat_response)
+    if not audio_output:
+        raise HTTPException(status_code=400, detail="Failed audio output")
+    def iterfile():
+       yield audio_output
+    return StreamingResponse(iterfile(), media_type="application/mpeg")
