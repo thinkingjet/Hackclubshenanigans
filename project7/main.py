04616909a3ddc9fb17523a17cb29363f0502f20d
Gauravjeet Singh <75510530+thinkingjet@users.noreply.github.com>
2024-07-01 07:01:28 +0000

    Implement Basic Player Movement

--- project7/main.py
+++ project7/main.py
@@ -10,7 +10,7 @@ def init():
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5)
+    glTranslatef(0.0, -1, -10)
     glEnable(GL_DEPTH_TEST)
 
 # Define the vertices and edges of a cube
@@ -65,6 +65,22 @@ def draw_maze():
                 draw_cube()
                 glPopMatrix()
 
+# Player position
+player_pos = [2, 0, 2]
+
+def draw_player():
+    glPushMatrix()
+    glTranslatef(player_pos[0], player_pos[1], player_pos[2])
+    glColor3f(1, 0, 0)  # Red color for player
+    draw_cube()
+    glPopMatrix()
+
+# Check for collisions
+def can_move(x, z):
+    if maze_layout[int(z / 2)][int(x / 2)] == 0:
+        return True
+    return False
+
 # Main loop
 def main():
     init()
@@ -74,8 +90,23 @@ def main():
                 pygame.quit()
                 quit()
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            if can_move(player_pos[0] - 0.1, player_pos[2]):
+                player_pos[0] -= 0.1
+        if keys[pygame.K_RIGHT]:
+            if can_move(player_pos[0] + 0.1, player_pos[2]):
+                player_pos[0] += 0.1
+        if keys[pygame.K_UP]:
+            if can_move(player_pos[0], player_pos[2] - 0.1):
+                player_pos[2] -= 0.1
+        if keys[pygame.K_DOWN]:
+            if can_move(player_pos[0], player_pos[2] + 0.1):
+                player_pos[2] += 0.1
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         draw_maze()
+        draw_player()
         pygame.display.flip()
         pygame.time.wait(10)
 
